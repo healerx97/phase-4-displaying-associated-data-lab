4127607f25175953c64d4317375cd1a0fdaa4cce
Kenny Yoon <ghun.yoon@gmail.com>
2021-07-22 00:57:14 -0400

    commit tree 3d37aedb8b926c2331f93c7e2a7a87e467ed7a20

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+    
+    def show
+        user = User.find_by(id: params['id'])
+        render json: user, include: :items
+    end
+
+    private
+
+    # users_params = params.permit(:username, :city)
+
 end
